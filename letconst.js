690aacefafce374e5f780c372c02ea5396e444e1
emrancub <myself.emranhasan@gmail.com>
2020-03-09 10:51:54 +0600

    First JavaScipt Code for Const & Let Variable

---
+++ letconst.js
@@ -0,0 +1,22 @@
+const hubby = "Purnima Apu";
+//hubby = "koel Mollik";
+console.log(hubby);
+
+const numbers = [14, 46];
+numbers[1]=88;
+numbers.push(12);
+numbers.pop();
+//numbers=("Emran","Sumon","Robi");
+console.log(numbers);
+
+//const nayok = {name: "Gopal", phone=01921232};
+
+let = patientName = "Rahim Chaca";
+patientName = "Fatema Khala";
+console.log(patientName);
+
+let sum =0;
+for(let i = 0; i<10; i++){
+    sum = sum + i;
+}
+console.log(i);
